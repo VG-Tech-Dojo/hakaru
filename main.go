5fddc8952c97b472c8d12437737ecb8b957336d8
pei0804 <peeeei0804@gmail.com>
2018-11-08 15:18:28 +0900

    流すers

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	dataSourceName := "root:hakaru-pass@tcp(hakaru.czfodluoktkk.ap-northeast-1.rds.amazonaws.com:3306)/hakaru-db"
+	dataSourceName := "root:password@tcp(hakaru.czfodluoktkk.ap-northeast-1.rds.amazonaws.com:3306)/hakaru-db"
 	hakaruHandler := func(w http.ResponseWriter, r *http.Request) {
 		db, err := sql.Open("mysql", dataSourceName)
 		if err != nil {
