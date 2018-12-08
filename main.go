f1795431eb0281009efc3fc93f21aee301c0d21a
tohutohu <tohu.soy@gmail.com>
2018-12-08 16:37:20 +0900

    improve app

--- main.go
+++ main.go
@@ -1,50 +1,62 @@
 package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
 
 	"database/sql"
 
-	_ "github.com/go-sql-driver/mysql"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+var (
+	db *sql.DB
+)
+
+func hakaruHandler(w http.ResponseWriter, r *http.Request) {
+
+	stmt, e := db.Prepare("INSERT INTO eventlog(at, name, value) values(NOW(), ?, ?)")
+	if e != nil {
+		panic(e.Error())
+	}
+
+	defer stmt.Close()
+
+	name := r.URL.Query().Get("name")
+	value := r.URL.Query().Get("value")
+
+	_, _ = stmt.Exec(name, value)
+
+	origin := r.Header.Get("Origin")
+	if origin != "" {
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+	} else {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+	}
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+}
 
 func main() {
 	dataSourceName := os.Getenv("HAKARU_DATASOURCENAME")
 	if dataSourceName == "" {
 		dataSourceName = "root:hakaru-pass@tcp(127.0.0.1:13306)/hakaru-db"
+
 	}
 
-	hakaruHandler := func(w http.ResponseWriter, r *http.Request) {
-		db, err := sql.Open("mysql", dataSourceName)
-		if err != nil {
-			panic(err.Error())
-		}
-		defer db.Close()
-
-		stmt, e := db.Prepare("INSERT INTO eventlog(at, name, value) values(NOW(), ?, ?)")
-		if e != nil {
-			panic(e.Error())
-		}
-
-		defer stmt.Close()
-
-		name := r.URL.Query().Get("name")
-		value := r.URL.Query().Get("value")
-
-		_, _ = stmt.Exec(name, value)
-
-		origin := r.Header.Get("Origin")
-		if origin != "" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-		} else {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-		}
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Allow-Methods", "GET")
+	_db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic(err.Error())
 	}
+	defer _db.Close()
+
+	db = _db
+
+	db.SetMaxIdleConns(20)
+	db.SetMaxOpenConns(20)
 
 	http.HandleFunc("/hakaru", hakaruHandler)
 	http.HandleFunc("/ok", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(200) })
