d1d92b39a3592b74ca69a7a1a773fcf5515ab30c
tockn <s.taku.inthebox@gmail.com>
2018-12-09 09:49:59 +0900

    コネクションプール使う, create HakaruHandler

--- main.go
+++ main.go
@@ -1,56 +1,61 @@
 package main
 
 import (
-	"net/http"
-	"log"
-
 	"database/sql"
+	"log"
+	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-	"os"
 )
 
 func main() {
+
 	dataSourceName := os.Getenv("HAKARU_DATASOURCENAME")
 	if dataSourceName == "" {
 		dataSourceName = "root:hakaru-pass@tcp(127.0.0.1:13306)/hakaru-db"
 	}
-
-	hakaruHandler := func(w http.ResponseWriter, r *http.Request) {
-		db, err := sql.Open("mysql", dataSourceName)
-		if err != nil {
-			panic(err.Error())
-		}
-		defer db.Close()
-
-		stmt, e := db.Prepare("INSERT INTO eventlog(at, name, value) values(NOW(), ?, ?)")
-		if e != nil {
-			panic(e.Error())
-		}
-
-		defer stmt.Close()
-
-		name := r.URL.Query().Get("name")
-		value := r.URL.Query().Get("value")
-
-		_, _ = stmt.Exec(name, value)
-
-		origin := r.Header.Get("Origin")
-		if origin != "" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-		} else {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-		}
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Allow-Methods", "GET")
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic(err.Error())
 	}
+	defer db.Close()
 
-	http.HandleFunc("/hakaru", hakaruHandler)
+	hakaru := HakaruHandler{DB: db}
+
+	http.Handle("/hakaru", hakaru)
 	http.HandleFunc("/ok", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(200) })
 
 	// start server
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type HakaruHandler struct {
+	DB *sql.DB
+}
+
+func (h HakaruHandler)ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	stmt, e := h.DB.Prepare("INSERT INTO eventlog(at, name, value) values(NOW(), ?, ?)")
+	if e != nil {
+		panic(e.Error())
+	}
+
+	defer stmt.Close()
+
+	name := r.URL.Query().Get("name")
+	value := r.URL.Query().Get("value")
+
+	_, _ = stmt.Exec(name, value)
+
+	origin := r.Header.Get("Origin")
+	if origin != "" {
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+	} else {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+	}
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
 }
