7ea2823c9872bdeac5cbd52bfa9f07bd09fb9c01
tohutohu <tohu.soy@gmail.com>
2018-12-09 10:44:59 +0900

    bulk insert

--- main.go
+++ main.go
@@ -3,31 +3,75 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
-	"database/sql"
+	"github.com/jmoiron/sqlx"
 
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type Value struct {
+	Name  string    `db:"name"`
+	Value string    `db:"value"`
+	Now   time.Time `db:"now"`
+}
+
 var (
-	db *sql.DB
+	db    *sqlx.DB
+	queCh = make(chan Value, 1000)
 )
 
-func hakaruHandler(w http.ResponseWriter, r *http.Request) {
+func inserter() {
+	ticker := time.NewTicker(1 * time.Second)
+	valueQue := make([]Value, 0, 1000)
+	for {
+		select {
+		case <-ticker.C:
+			// TODO: Insert 処理
+			//stmt, e := db.Prepare("INSERT INTO eventlog(at, name, value) values(NOW(), ?, ?)")
+			//if e != nil {
+			//	panic(e.Error())
+			//}
+			if len(valueQue) == 0 {
+				continue
+			}
+			query := "INSERT INTO eventlog(at, name, value) values(?, ?, ?)" + strings.Repeat(", (?, ?, ?)", len(valueQue)-1)
+			stmt, e := db.Prepare(query)
+			if e != nil {
+				panic(e.Error())
+			}
 
-	stmt, e := db.Prepare("INSERT INTO eventlog(at, name, value) values(NOW(), ?, ?)")
-	if e != nil {
-		panic(e.Error())
+			defer stmt.Close()
+
+			args := make([]interface{}, 3*len(valueQue))
+			for i, que := range valueQue {
+				args[i] = que.Now
+				args[i+1] = que.Name
+				args[i+2] = que.Value
+			}
+			_, _ = stmt.Exec(args...)
+			valueQue = make([]Value, 0, 1000)
+
+		case que := <-queCh:
+			valueQue = append(valueQue, que)
+
+		}
 	}
+}
 
-	defer stmt.Close()
-
+func hakaruHandler(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
 	name := r.URL.Query().Get("name")
 	value := r.URL.Query().Get("value")
 
-	_, _ = stmt.Exec(name, value)
+	queCh <- Value{
+		Now:   now,
+		Name:  name,
+		Value: value,
+	}
 
 	origin := r.Header.Get("Origin")
 	if origin != "" {
@@ -47,7 +91,7 @@ func main() {
 
 	}
 
-	_db, err := sql.Open("mysql", dataSourceName)
+	_db, err := sqlx.Open("mysql", dataSourceName)
 	if err != nil {
 		panic(err.Error())
 	}
